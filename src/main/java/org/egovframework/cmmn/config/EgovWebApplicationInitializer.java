b2dc94f997418c11a32e78f0ef8d4c359c91aa58
Jooho Yoon <platonicojju@gmail.com>
2021-04-07 22:06:02 +0900

    작업중

---
+++ src/main/java/org/egovframework/cmmn/config/EgovWebApplicationInitializer.java
@@ -0,0 +1,89 @@
+package org.egovframework.cmmn.config;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.egovframework.cmmn.filter.HTMLTagFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * Class Name : EgovWebApplicationInitializer Description : <Notice> Servlet3.x
+ * 을 지원하기 위해 web.xml의 기능을 WebApplicationInitializer 기능으로 처리 </Notice>
+ * <Disclaimer> N/A
+ *
+ * @author 윤주호
+ * @since 2021.04.07
+ * @version 1.0
+ * @see
+ *
+ *      <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ *   수정일        수정자           수정내용
+ *  -------      -------------  ----------------------
+ *   2021.04.07  윤주호           최초 생성
+ *      </pre>
+ */
+
+public class EgovWebApplicationInitializer implements WebApplicationInitializer {
+	private static final Logger LOGGER = LoggerFactory.getLogger(EgovWebApplicationInitializer.class);
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		// -------------------------------------------------------------
+		// Egov Web ServletContextListener 설정
+		// -------------------------------------------------------------
+		servletContext.addListener(new org.egovframework.cmmn.context.EgovWebServletContextListener());
+
+		// -------------------------------------------------------------
+		// Spring CharacterEncodingFilter 설정
+		// -------------------------------------------------------------
+		FilterRegistration.Dynamic characterEncoding = servletContext.addFilter("encodingFilter",
+				new org.springframework.web.filter.CharacterEncodingFilter());
+		characterEncoding.setInitParameter("encoding", "UTF-8");
+		characterEncoding.setInitParameter("forceEncoding", "true");
+		characterEncoding.addMappingForUrlPatterns(null, false, "*.do");
+		// characterEncoding.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST),
+		// true, "*.do");
+
+		// -------------------------------------------------------------
+		// Spring ServletContextListener 설정
+		// -------------------------------------------------------------
+		XmlWebApplicationContext rootContext = new XmlWebApplicationContext();
+		rootContext.setConfigLocations(new String[] { "classpath*:egovframework/spring/com/**/context-*.xml" });
+		// rootContext.setConfigLocations(new String[] {
+		// "classpath*:egovframework/spring/com/context-*.xml","classpath*:egovframework/spring/com/*/context-*.xml"
+		// });
+		rootContext.refresh();
+		rootContext.start();
+
+		servletContext.addListener(new ContextLoaderListener(rootContext));
+		
+		// -------------------------------------------------------------
+		// Spring ServletContextListener 설정
+		// -------------------------------------------------------------
+		XmlWebApplicationContext xmlWebApplicationContext = new XmlWebApplicationContext();
+		xmlWebApplicationContext.setConfigLocation("/WEB-INF/config/egovframework/springmvc/egov-com-*.xml");
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher",
+				new DispatcherServlet(xmlWebApplicationContext));
+		// dispatcher.addMapping("*.do");
+		dispatcher.addMapping("/"); // Facebook OAuth 사용시 변경
+		dispatcher.setLoadOnStartup(1);
+
+		//-------------------------------------------------------------
+	    // HTMLTagFilter의 경우는 파라미터에 대하여 XSS 오류 방지를 위한 변환을 처리합니다.
+		//-------------------------------------------------------------	
+	    // HTMLTagFIlter의 경우는 JSP의 <c:out /> 등을 사용하지 못하는 특수한 상황에서 사용하시면 됩니다.
+	    // (<c:out />의 경우 뷰단에서 데이터 출력시 XSS 방지 처리가 됨)
+		FilterRegistration.Dynamic htmlTagFilter = servletContext.addFilter("htmlTagFilter", new HTMLTagFilter());
+		htmlTagFilter.addMappingForUrlPatterns(null, false, "*.do");
+	}
+
+}
---
+++ src/main/java/org/egovframework/cmmn/context/EgovWebServletContextListener.java
@@ -0,0 +1,63 @@
+package org.egovframework.cmmn.context;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.egovframework.cmmn.service.EgovProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class Name : EgovWebServletContextListener  
+ * Description : <Notice> 데이터베이스 설정을
+ * spring.profiles.active 방식으로 처리 (공통컴포넌트 특성상 데이터베이스별 분리/개발,검증,운영서버로 분리 가능)
+ * <Disclaimer> N/A
+ *
+ * @author 윤주호
+ * @since 2021.04.07
+ * @version 1.0
+ * @see
+ *
+ *      <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ *   수정일        수정자           수정내용
+ *  -------      -------------  ----------------------
+ *   2021.04.07  윤주호           최초 생성
+ *      </pre>
+ */
+public class EgovWebServletContextListener implements ServletContextListener {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(EgovWebServletContextListener.class);
+
+	public EgovWebServletContextListener() {
+		setEgovProfileSetting();
+	}
+
+	public void contextInitialized(ServletContextEvent event) {
+		if (System.getProperty("spring.profiles.active") == null) {
+			setEgovProfileSetting();
+		}
+	}
+
+	public void contextDestroyed(ServletContextEvent event) {
+		if (System.getProperty("spring.profiles.active") != null) {
+			System.setProperty("spring.profiles.active", null);
+		}
+	}
+
+	public void setEgovProfileSetting() {
+		try {
+			LOGGER.debug("===========================Start EgovServletContextLoad START ===========");
+			System.setProperty("spring.profiles.active",
+					EgovProperties.getProperty("Globals.DbType") + "," + EgovProperties.getProperty("Globals.Auth"));
+			LOGGER.debug("Setting spring.profiles.active>" + System.getProperty("spring.profiles.active"));
+			LOGGER.debug("===========================END   EgovServletContextLoad END ===========");
+			// 2017.03.03 조성원 시큐어코딩(ES)-오류 메시지를 통한 정보노출[CWE-209]
+		} catch (IllegalArgumentException e) {
+			LOGGER.error("[IllegalArgumentException] Try/Catch...usingParameters Runing : " + e.getMessage());
+		} catch (Exception e) {
+			LOGGER.error("[" + e.getClass() + "] search fail : " + e.getMessage());
+		}
+	}
+}
